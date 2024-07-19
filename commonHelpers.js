18d57e46a1b59716652f47db463e2554aeb1158c
NicolaiDodeac <163674087+NicolaiDodeac@users.noreply.github.com>
2024-07-19 17:26:46 +0000

    Deploying to gh-pages from @ NicolaiDodeac/goit-js-hw-11@843013b085033f8fa0c49da096e5a5f6c20bb567 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,20 +1,20 @@
-import{i as a}from"./assets/vendor-ad859c2f.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&s(n)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const l=document.querySelector(".searchForm"),c=document.querySelector(".gallery");l.addEventListener("submit",u);function u(o){o.preventDefault();const r=o.target.elements.search.value.trim();if(r==="")return;const s=`https://pixabay.com/api/?key=44985278-910018cc950880488ff0b70a1&q=${r}&image_type=photo&orientation=horizontal&safesearch=true`;fetch(s).then(e=>{if(!e.ok)throw new Error("Network response was not ok");return e.json()}).then(e=>{e.hits.length>0?f(e.hits):a.error({title:"",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",color:"red"})}).catch(e=>{a.error({title:"Error",message:"Something went wrong. Please try again later.",position:"topRight",color:"red"})})}function f(o){c.innerHTML="",l.reset();const r=o.map(d).join("");c.insertAdjacentHTML("beforeend",r)}function d({webformatURL:o,largeImageURL:r,tags:i,likes:s,views:e,comments:t,downloads:n}){return`
+import{S as c,i as l}from"./assets/vendor-8c59ed88.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();function u(s,r){r.innerHTML="";const o=s.map(f).join("");r.insertAdjacentHTML("beforeend",o)}function f({webformatURL:s,largeImageURL:r,tags:o,likes:n,views:e,comments:t,downloads:i}){return`
     <li class="photo-card">
       <a class="gallery-link" href="${r}">
         <img
-          src="${o}"
-          alt="${i}"
+          src="${s}"
+          alt="${o}"
           loading="lazy"
         />
       </a>
       <div class="wrapper">
         <div class="info">
-          <b class="student-info">Likes: ${s}</b>
+          <b class="student-info">Likes: ${n}</b>
           <b class="student-info">Views: ${e}</b>
           <b class="student-info">Comments: ${t}</b>
-          <b class="student-info">Downloads: ${n}</b>
+          <b class="student-info">Downloads: ${i}</b>
         </div>
       </div>
     </li>
-  `}
+  `}const d="44985278-910018cc950880488ff0b70a1";function h(s){const r=`https://pixabay.com/api/?key=${d}&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`;return fetch(r).then(o=>{if(!o.ok)throw new Error("Network response was not ok");return o.json()})}const a={searchFormRef:document.querySelector(".searchForm"),galleryEl:document.querySelector(".gallery"),loader:document.querySelector(".loader")};a.searchFormRef.addEventListener("submit",m);function m(s){s.preventDefault();const r=s.target.elements.search.value.trim();r!==""&&(a.loader.style.display="block",h(r).then(o=>{o.hits.length>0?(u(o.hits,a.galleryEl),a.searchFormRef.reset(),new c(".gallery a",{captionsData:"alt"}).refresh()):l.error({title:"",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",color:"red"})}).catch(o=>{l.error({title:"Error",message:"Something went wrong. Please try again later.",position:"topRight",color:"red"})}).finally(()=>{a.loader.style.display="none"}))}
 //# sourceMappingURL=commonHelpers.js.map
